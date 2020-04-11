dd786e3f22ace9ad107ecaa4735b90f2b2e23747
dos Santos, Diego <diego.dos.santos@sap.com>
2020-04-10 22:09:37 -0300

    Added tests for standard Deviation service

--- api/services/standardDeviation.js
+++ api/services/standardDeviation.js
@@ -2,15 +2,24 @@ module.exports = () => {
   const standardDeviation = {};
 
   standardDeviation.calculate = (numbers) => {
+    var result = {
+      "mean": "0.00",
+      "standardDeviation": "0.00"
+    };
+
+    if (!Array.isArray(numbers)) {
+      return result;
+    }
+
     const count = numbers.length;
     const mean = numbers.reduce((total, value) => total + value / count, 0);
     const variance = numbers.reduce((total, value) => total + Math.pow(mean - value, 2) / (count - 1), 0);
     const stdDeviation = Math.sqrt(variance);
 
-    return {
-      "mean": mean.toFixed(2),
-      "standardDeviation": stdDeviation.toFixed(2)
-    };
+    result.mean = mean.toFixed(2);
+    result.standardDeviation = stdDeviation.toFixed(2);
+
+    return result;
   };
 
   return standardDeviation;
